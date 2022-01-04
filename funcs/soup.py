c16897eaeea79543e69c8a717e1d9e08e30b9761
Marcelo Junior <marcelo.junior@aluno.ic.ufmt.br>
2022-01-04 19:17:31 -0400

    Revert "Fast version changes"
    This reverts commit 3faa65931fc02330023e66efe8e11016b28ad688.

--- funcs/soup.py
+++ funcs/soup.py
@@ -20,7 +20,9 @@ def getStockPrice(stocks, stocksPrice, running):
                     stocksPrice[i] = (sign if sign else '') + main_div.text
                 except:
                     stocksPrice[i] = "Error"
-                #welp, this is the fast version, so whatever
+                #i'm afraid of sending too many requests too quickly and being flagged as a DDoS attacker :/
+                #time.sleep(1)
+            time.sleep(1.5)
         except IndexError:
             pass
         except:
